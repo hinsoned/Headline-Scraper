4bdc51bd920fd2f375769931ebc65b336c636751
hinsoned <102074110+hinsoned@users.noreply.github.com>
2025-09-12 14:48:45 -0400

    Topics to CSV
    - Added a topic column to the data frame and CSV
    
    - Moved urls to global scope in anticipation of adding more sites to scrape

--- app.py
+++ app.py
@@ -5,11 +5,24 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
 
+# CNN URLs and topics
+CNN_URLs = [
+        {"url": "https://www.cnn.com", "topic": "General"},
+        {"url": "https://www.cnn.com/us", "topic": "US"},
+        {"url": "https://www.cnn.com/world", "topic": "World"},
+        {"url": "https://www.cnn.com/business", "topic": "Business"},
+        {"url": "https://www.cnn.com/politics", "topic": "Politics"},
+        {"url": "https://www.cnn.com/health", "topic": "Health"},
+        {"url": "https://www.cnn.com/entertainment", "topic": "Entertainment"},
+    ]
+
+# Print headlines
 def print_headlines(headlines, url):
     print(f"{len(headlines)} headlines found on {url}")
     for index, headline in enumerate(headlines, 1):
         print(f"{index}. {headline}")
 
+# Get headlines
 def get_headlines(url):
     print(f"Scraping {url}...")
 
@@ -20,24 +33,18 @@ def get_headlines(url):
     for headline in soup.find_all(["span"], class_="container__headline-text"):
         headlines.append(headline.text)
 
-    #print_headlines(headlines)
     return headlines
 
+# Main function
 def main():
-    CNN_URLs = ["https://www.cnn.com", 
-    "https://www.cnn.com/us", 
-    "https://www.cnn.com/world", 
-    "https://www.cnn.com/business", 
-    "https://www.cnn.com/politics", 
-    "https://www.cnn.com/health", 
-    "https://www.cnn.com/entertainment",
-    ]
-
     all_data = []
 
-    for URL in CNN_URLs:
-        headlines = get_headlines(URL)
-        print_headlines(headlines, URL)
+    for entry in CNN_URLs:
+        url = entry["url"]
+        topic = entry["topic"]
+
+        headlines = get_headlines(url)
+        print_headlines(headlines, url)
         print("\n")
 
         timestamp = datetime.now()
@@ -45,7 +52,8 @@ def main():
             all_data.append({
                 "timestamp": timestamp,
                 "headline": headline,
-                "url": URL
+                "url": url,
+                "topic": topic
             })
 
     df = pd.DataFrame(all_data)
